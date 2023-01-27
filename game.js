bd246623240291cbfca1811144b96f99d5266169
Daniel Cuevas <ivandaniel1224@gmail.com>
2023-01-27 08:53:25 -0500

    Canvas size

--- game.js
+++ game.js
@@ -1,1 +1,17 @@
+const canvas = document.querySelector("#game");
+const game = canvas.getContext("2d");
 
+window.addEventListener("load", startGame);
+
+function startGame() {
+  let canvasSize;
+
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8;
+  }
+
+  canvas.setAttribute("width", canvasSize);
+  canvas.setAttribute("height", canvasSize);
+}
