d347e0b33058bd51bdf57eac66817c94be5bd7d1
Daniel Cuevas <ivandaniel1224@gmail.com>
2023-01-27 11:08:49 -0500

    Render elements on the map

--- game.js
+++ game.js
@@ -26,7 +26,16 @@ function startGame() {
   game.font = elementsSize + "px Verdana";
   game.textAlign = "end";
 
-  for (let i = 1; i <= 10; i++) {
-    game.fillText(emojis["X"], elementsSize, elementsSize * i);
-  }
+  const map = maps[0];
+  const mapRows = map.trim().split("\n");
+  const mapRowCols = mapRows.map((row) => row.trim().split(""));
+
+  mapRowCols.forEach((row, rowIndex) => {
+    row.forEach((col, colIndex) => {
+      const emoji = emojis[col];
+      const posX = elementsSize * (colIndex + 1);
+      const posY = elementsSize * (rowIndex + 1);
+      game.fillText(emoji, posX, posY);
+    });
+  });
 }
