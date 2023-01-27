90219acb0fd7ce1535e6515784aa60f2c3a37c05
Daniel Cuevas <ivandaniel1224@gmail.com>
2023-01-27 09:05:39 -0500

    Elements size

--- game.js
+++ game.js
@@ -7,11 +7,20 @@ function startGame() {
   let canvasSize;
 
   if (window.innerHeight > window.innerWidth) {
-    canvasSize = window.innerWidth * 0.8;
+    canvasSize = window.innerWidth * 0.75;
   } else {
-    canvasSize = window.innerHeight * 0.8;
+    canvasSize = window.innerHeight * 0.75;
   }
 
   canvas.setAttribute("width", canvasSize);
   canvas.setAttribute("height", canvasSize);
+
+  const elementsSize = canvasSize / 10;
+
+  game.font = elementsSize + "px Verdana";
+  game.textAlign = "end";
+
+  for (let i = 1; i <= 10; i++) {
+    game.fillText(emojis["X"], elementsSize, elementsSize * i);
+  }
 }
