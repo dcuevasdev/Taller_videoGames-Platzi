6c4c19107a2d936c030cb1aaa4f0abb8d00c8be7
Daniel Cuevas <ivandaniel1224@gmail.com>
2023-01-31 18:41:10 -0500

    Player movement

--- game.js
+++ game.js
@@ -1,8 +1,17 @@
 const canvas = document.querySelector("#game");
 const game = canvas.getContext("2d");
+const btnUp = document.querySelector("#up");
+const btnLeft = document.querySelector("#left");
+const btnRight = document.querySelector("#right");
+const btnDown = document.querySelector("#down");
 
 let canvasSize;
 let elementsSize;
+
+const playerPosition = {
+  x: undefined,
+  y: undefined,
+};
 
 window.addEventListener("load", setCanvasSize);
 window.addEventListener("resize", setCanvasSize);
@@ -30,12 +39,69 @@ function startGame() {
   const mapRows = map.trim().split("\n");
   const mapRowCols = mapRows.map((row) => row.trim().split(""));
 
+  game.clearRect(0, 0, canvasSize, canvasSize);
+
   mapRowCols.forEach((row, rowIndex) => {
     row.forEach((col, colIndex) => {
       const emoji = emojis[col];
       const posX = elementsSize * (colIndex + 1);
       const posY = elementsSize * (rowIndex + 1);
+
+      if (col == "O") {
+        if (!playerPosition.x && !playerPosition.y) {
+          playerPosition.x = posX;
+          playerPosition.y = posY;
+        }
+      }
+
       game.fillText(emoji, posX, posY);
     });
   });
+
+  movePlayer();
+}
+
+function movePlayer() {
+  game.fillText(emojis["PLAYER"], playerPosition.x, playerPosition.y);
+}
+
+window.addEventListener("keydown", moveByKeys);
+btnUp.addEventListener("click", moveUp);
+btnLeft.addEventListener("click", moveLeft);
+btnRight.addEventListener("click", moveRight);
+btnDown.addEventListener("click", moveDown);
+
+function moveByKeys(event) {
+  switch (event.key) {
+    case "ArrowUp":
+      moveUp();
+      break;
+    case "ArrowLeft":
+      moveLeft();
+      break;
+    case "ArrowRight":
+      moveRight();
+      break;
+    case "ArrowDown":
+      moveDown();
+      break;
+    default:
+      break;
+  }
+}
+function moveUp() {
+  playerPosition.y -= elementsSize;
+  startGame();
+}
+function moveLeft() {
+  playerPosition.x -= elementsSize;
+  startGame();
+}
+function moveRight() {
+  playerPosition.x += elementsSize;
+  startGame();
+}
+function moveDown() {
+  playerPosition.y += elementsSize;
+  startGame();
 }
